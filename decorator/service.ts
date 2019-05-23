c413819f3cddac96ac7f18caee476d25b4e3487b
gongul <pyeonbun@naver.com>
2019-05-23 15:18:06 +0900

    add service decorator

---
+++ decorator/service.ts
@@ -0,0 +1,17 @@
+import { ClassType } from "type-graphql";
+import { DescriptionOptions } from "type-graphql/dist/decorators/types";
+import { Interface } from "readline";
+
+
+const Service = (beanName:string):Function => {  
+    return (target:any) => {
+        console.log(target);
+        // console.log(propertyKey);
+        // console.log(descriptor);
+        // target[propertyKey] = new injectionClass();
+
+        // descriptor.ㄷ
+    }
+}
+
+export {Service}
